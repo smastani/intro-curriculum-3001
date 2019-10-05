5d6f074e0d7565881fc9a650dabb57a98d96a240
Satoshi Masutani <smastani@w2.dion.ne.jp>
2019-10-06 00:29:30 +0900

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
 */
 function factorial(n) {
     let result = 1;
-    // TODO このコメントを消して正しく実装してください。
+    for (let i = 1; i <= Number; i++){}
+        result = result * i;
+    }   
+    console.log(result);
     return result;
 }
 const assert = require('assert');
