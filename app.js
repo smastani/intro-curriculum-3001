a294c4d1ac261bfe96e17b525164e50d5e503aa8
Satoshi Masutani <smastani@w2.dion.ne.jp>
2019-10-08 20:08:37 +0900

    階乗関数の実装

--- app.js
+++ app.js
@@ -7,10 +7,9 @@
 */
 function factorial(n) {
     let result = 1;
-    for (let i = 1; i <= Number; i++){}
+    for (let i = 1; i <= n; i++){
         result = result * i;
     }   
-    console.log(result);
     return result;
 }
 const assert = require('assert');
